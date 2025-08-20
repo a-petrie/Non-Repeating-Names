e0cbc9344843eb90bd28ae2625a8045830f35b3d
Allan Petrie <apetrie@mathworks.com>
2025-08-21 00:17:33 +0100

    add implementation with pre-filtering

---
+++ pre-filter/non_repeating.py
@@ -0,0 +1,36 @@
+from itertools import product
+
+def is_ascii(s):
+    return all(ord(c) < 128 for c in s)
+
+def read_name_dataset(dataset_file: str) -> [str]:
+    with open(dataset_file) as f:
+        names = [name.strip() for name in f.readlines()]
+    return [name for name in names if is_ascii(name)]
+
+def has_no_repeating_characters(name: str) -> bool:
+    return len(name) == len(set(name))
+
+if __name__ == "__main__":
+
+    first_names = read_name_dataset("../first_names.all.txt")
+    last_names = read_name_dataset("../last_names.all.txt")
+
+    first_names = [name for name in first_names if has_no_repeating_characters(name)]
+    last_names = [name for name in last_names if has_no_repeating_characters(name)]
+
+    print(f"{len(first_names)} * {len(last_names)} = {len(first_names) * len(last_names)}")
+
+    """
+    non_repeating_names = []
+
+    
+    for first, last in product(first_names, last_names):
+        name = f"{first} {last}"
+        if has_no_repeating_characters(name):
+            print(f"{name} has no repeating characters")
+            non_repeating_names.append(name)        
+
+    with open("result.txt", "w") as f:
+        f.writelines(non_repeating_names)
+    """
