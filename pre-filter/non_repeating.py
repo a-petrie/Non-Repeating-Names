0a0d2194f2cbf87d9543d864102e49afb5b87183
Allan Petrie <apetrie@mathworks.com>
2025-08-21 16:09:38 +0100

    update pre-filter to use generators as well

--- pre-filter/non_repeating.py
+++ pre-filter/non_repeating.py
@@ -3,25 +3,18 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'common')))
 
-from utils import read_name_dataset, is_ascii, has_no_repeating_characters
+from utils import load_dataset, is_ascii, has_no_repeating_characters
 
 from itertools import product
 
 if __name__ == "__main__":
-
-    first_names = read_name_dataset("../first_names.all.txt")
-    last_names = read_name_dataset("../last_names.all.txt")
-
+    first_names, last_names = load_dataset()
     first_names = [name for name in first_names if has_no_repeating_characters(name)]
     last_names = [name for name in last_names if has_no_repeating_characters(name)]
 
-    non_repeating_names = []
-    
-    for first, last in product(first_names, last_names):
-        name = f"{first} {last}"
-        if has_no_repeating_characters(name):
-            print(f"{name} has no repeating characters")
-            non_repeating_names.append(name)        
+    names = (f"{first} {last}" for first, last in product(first_names, last_names))
+    non_repeating_names = (name for name in names if has_no_repeating_characters(name))
 
     with open("result.txt", "w") as f:
-        f.writelines(non_repeating_names)
+        for name in non_repeating_names:
+            f.write(name + "\n")
