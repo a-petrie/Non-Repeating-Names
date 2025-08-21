de141e1ab74903056aee68c5c5e7c33884e72e33
Allan Petrie <apetrie@mathworks.com>
2025-08-21 01:15:54 +0100

    add way to specify what dir to load dataset from

--- common/utils.py
+++ common/utils.py
@@ -3,6 +3,9 @@ def read_name_dataset(dataset_file: str) -> [str]:
         names = [name.strip() for name in f.readlines()]
     return [name for name in names if is_ascii(name)]
 
+def load_dataset(rel_dir = "..") -> ([str], [str]):
+    return read_name_dataset(f"{rel_dir}/first_names.all.txt"), read_name_dataset(f"{rel_dir}/last_names.all.txt")
+
 def is_ascii(s):
     return all(ord(c) < 128 for c in s)
 
