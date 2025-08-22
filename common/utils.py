e49a58623177626c06a3f0822e152f3bc5e33631
Allan Petrie <apetrie@mathworks.com>
2025-08-22 15:40:20 +0100

    introduce sample_size parameter when loading datasets

--- common/utils.py
+++ common/utils.py
@@ -1,10 +1,23 @@
+from random import sample
+
+FIRST_NAMES = "first_names.all.txt"
+LAST_NAMES = "last_names.all.txt"
+
 def read_name_dataset(dataset_file: str) -> [str]:
     with open(dataset_file) as f:
         names = [name.strip() for name in f.readlines()]
     return [name for name in names if is_ascii(name)]
 
-def load_dataset() -> ([str], [str]):
-    return read_name_dataset(f"../first_names.all.txt"), read_name_dataset(f"../last_names.all.txt")
+def load_dataset(sample_size=None) -> ([str], [str]):
+    first_names = read_name_dataset(f"../{FIRST_NAMES}")
+    last_names =read_name_dataset(f"../{LAST_NAMES}")
+
+    if sample_size is None:
+        return first_names, last_names
+    return (
+        sample(first_names, sample_size),
+        sample(last_names, sample_size)
+    )
 
 def is_ascii(s):
     return all(ord(c) < 128 for c in s)
