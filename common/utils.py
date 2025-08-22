c168bd9e4daf2ce92eef41c2ce4929e0acedeca7
Allan Petrie <apetrie@mathworks.com>
2025-08-22 20:13:01 +0100

    no need to call is_ascii since we've already done this in pre-cleanup

--- common/utils.py
+++ common/utils.py
@@ -5,8 +5,7 @@ LAST_NAMES = "last_names.all.txt"
 
 def read_name_dataset(dataset_file: str) -> [str]:
     with open(dataset_file) as f:
-        names = [name.strip() for name in f.readlines()]
-    return [name for name in names if is_ascii(name)]
+        return [name.strip() for name in f.readlines()]
 
 def load_dataset(sample_size=None) -> ([str], [str]):
     first_names = read_name_dataset(f"../{FIRST_NAMES}")
@@ -18,9 +17,6 @@ def load_dataset(sample_size=None) -> ([str], [str]):
         sample(first_names, sample_size),
         sample(last_names, sample_size)
     )
-
-def is_ascii(s):
-    return all(ord(c) < 128 for c in s)
 
 def has_no_repeating_characters(name: str) -> bool:
     return len(name) == len(set(name))
